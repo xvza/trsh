b528723e58625c8ba40b536f89af4cae67eb27d0
xvza <>
2020-04-12 17:08:54 +0200

    trsh

---
+++ srcs/flatpak.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+############################
+declare   "del=uninstall -y"
+declare   "add=install -y"
+declare   "upd=update -y"
+############################
+declare   "li=list"
+############################
---
+++ srcs/yay.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+########################################
+declare   "del=-Rnscu --noconfirm"
+declare   "add=-Sq --needed --noconfirm"
+declare   "upd=-Sqyu --noconfirm"
+########################################
+declare   "rep=-Qmq"
+declare   "orp=-Qdtq"
+########################################
