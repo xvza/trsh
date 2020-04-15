25379f62b070add12ddb1106885cdba186c42eb0
xvza <>
2020-04-15 17:29:31 +0200

    trsh

--- srcs/picom.sh
+++ srcs/picom.sh
@@ -2,7 +2,7 @@
 #################################################
 declare   "name=$(basename ${BASH_SOURCE[0]%.*})"
 declare   "cmd=killall"
-declare   "cfg=${SHARE}/${name}/config.ini"
+declare   "cfg=${SHRD}/${name}/config.ini"
 #################################################
 eval      "${cmd} -q ${name}"
 eval      "${name} --config ${cfg}"             &
